878c93508e5f8b0d61acbd46b9ca443d8212139a
GabrielLopeszx <147755984+GabrielLopeszx@users.noreply.github.com>
2023-10-16 18:39:31 -0300

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,14 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import './ExpenseTotal.css';
+
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
-    }, 0);
+    const { currency, dispatch } = useContext(AppContext);
+
+    const currencyOptions = [
+        { value: 'dollar', label: '$ Dollar' },
+        { value: 'pound', label: '£ Pound' },
+        { value: 'euro', label: '€ Euro' },
+        { value: 'ruppee', label: '₹ Rupee' },
+    ];
+
+    const handleCurrencyChange = (selectedCurrency) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: selectedCurrency,
+        });
+    };
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(
+        currencyOptions.find((option) => option.value === currency)
+    );
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const selectOption = (option) => {
+        setSelectedOption(option);
+        handleCurrencyChange(option.value);
+        setIsDropdownOpen(false);
+    };
+
     return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+        <div className='yellow-dropdown' onClick={toggleDropdown}>
+            Currency: {selectedOption.label}
+            {isDropdownOpen && (
+                <div className='dropdown-options'>
+                    {currencyOptions.map((option) => (
+                        <div key={option.value} onClick={() => selectOption(option)}>
+                            {option.label}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
+
 export default ExpenseTotal;
